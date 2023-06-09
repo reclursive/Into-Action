676e767888dc9b1a1f4e9df32e070d1513540f1b
reclursive <imafraidofferrets@gmail.com>
2023-06-08 19:56:57 -0700

    js intro datatypes

---
+++ lesson-notes/js-intro.js
@@ -0,0 +1,78 @@
+//datatypes in Javascript
+
+//integers or numbers, strings, arrays, objects
+
+//GROK this material
+
+//variables ES5 (just var) ES6 (const + let)
+
+
+
+//const is variable that will stay the same
+//let is setting a variable with a temporary variable (score let score =0)
+
+//numbers
+const x = 2
+
+const y = 4
+
+const multiplied = x * y 
+
+
+
+//strings
+const kerstinJoe = "kerstin"
+
+
+
+//array (any datatype, but all same datatype)
+const pokemon = ["pikachu","9", "bulbasaur", "charzard"]
+
+const scores = [0,5,1,2]
+
+
+//INDEXES IN ARRAYS
+
+
+//arrray indexes ALL begin at 0
+
+//access anything within an array using [BRACKET NOTATION]
+// console.log(scores[1]+scores[3])
+
+//concatenation combining 
+
+// console.log(kerstinJoe + " is Data")
+
+const user1 = {
+    username: "Blob",
+    password: "catsAreMyLife13!",
+    passcode: 4400
+}
+
+const user2 = {
+    username: "Bleeb",
+    password: "catsAreMyLife14!",
+    passcode: 7777
+}
+const user3 = {
+    username: "Blonk",
+    password: "catsAreMyLife15!",
+    passcode: 6666
+}
+
+
+const users = [user1, user2, user3]
+
+//get any individual piece of info from an object with DOT NOTATION
+
+console.log(users[1].passcode)
+
+
+
+
+
+
+
+
+
+

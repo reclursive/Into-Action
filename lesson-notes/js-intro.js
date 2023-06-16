454a10ed0a05b36a975f941c0b1d3b53731f373c
reclursive <imafraidofferrets@gmail.com>
2023-06-15 19:59:33 -0700

    Functions deep dive, assignment and comparison  opperators

--- lesson-notes/js-intro.js
+++ lesson-notes/js-intro.js
@@ -18,6 +18,13 @@ const y = 4
 
 const multiplied = x * y 
 
+//booleans (late to the party lol)
+//true and false
+//1    and    0 
+
+const alive = true;
+
+const dead = false;
 
 
 //strings
@@ -28,11 +35,31 @@ const kerstinJoe = "kerstin"
 //array (any datatype, but all same datatype)
 const pokemon = ["pikachu","9", "bulbasaur", "charzard"]
 
-const scores = [0,5,1,2]
+const scores = []
+
+// console.log(scores[0])
+
+///== === =
+
+// =        Setting a value to something
+
+//below totally different because they are checks
+// == loosy goosy
+// === YOU CAN"T SIT WITH USSSSSS
+
+
+// == looser equals (1 = "1")
+// ===  STRICT equal (down to data type)
+// = Assignment operator (not used for conditionals, used for ASSIGNING)
+
+
+// comparison Operators 
+
+// && both have to be true
+// || one has to be true
 
 
 //INDEXES IN ARRAYS
-
 
 //arrray indexes ALL begin at 0
 
@@ -65,7 +92,62 @@ const users = [user1, user2, user3]
 
 //get any individual piece of info from an object with DOT NOTATION
 
-console.log(users[1].passcode)
+// console.log(users[1].passcode)
+
+
+
+
+//putting the FUN in functions
+
+//conditionals (NOT MATH)
+
+
+//if it's sunny- i'll wear sunscreen. If it's rainy- I'll bring my umbrella.
+//otherwise - I'll wear my PJs <----
+
+//
+
+
+// if sunshine true status = "ill"
+
+function checkStatus(sunshine){
+    let status = ""
+
+    if (sunshine === true){
+        status = "I'll wear sunscreen"
+    }
+    else if (sunshine === false){
+        status = "I'll bring an umbrella"
+    }
+    else {
+        status = "I'll wear my PJss"
+    }
+    console.log("this is the status " + status)
+}
+
+checkStatus(true)
+
+
+
+
+
+
+// checkStatus(false)
+
+// checkStatus("gloomy")
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

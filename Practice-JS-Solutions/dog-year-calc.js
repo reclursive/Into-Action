d1baf0700167cdd053d531908f55ab7e4439874e
kerstin-c <klcynkar@gmail.com>
2023-07-17 15:06:01 -0700

    attempting dog years in js

---
+++ Practice-JS-Solutions/dog-year-calc.js
@@ -0,0 +1,95 @@
+//conditionals (NOT MATH)
+
+//if it's sunny- i'll wear sunscreen. If it's rainy- I'll bring my umbrella.
+//otherwise - I'll wear my PJs <----
+
+//
+
+// if sunshine true status = "ill"
+
+const dogAge = (8) => {
+  let ageAdjust = "";
+
+  if (dogAge === >2) {
+ ageAdjust = (2 * 10.5);
+  } else if (sunshine === false) {
+ ageAdjust = "I'll bring an umbrella";
+  } else {
+ ageAdjust = "I'll wear my PJss";
+  }
+};
+
+// checkStatus(true)
+
+const greeting = (formality, timeDay) => {
+  if (
+    (formality === "job fair" ||
+      formality === "interview" ||
+      formality === "meeting professor") &&
+    timeDay === "morning"
+  ) {
+    console.log("Good morning");
+  } else if (formality === "friends" && timeDay === "afternoon") {
+    console.log("what's up");
+  } else if (formality === "interview" && timeDay === "evening") {
+    console.log("Good evening");
+  } else {
+    console.log("none of the above");
+  }
+};
+
+greeting("job fair", "afternoon");
+
+// checkStatus(false)
+
+// checkStatus("gloomy")
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//How old is my dog in human years
+
+const dogAgeCalculator = (8) => {
+  if (dogAge === 
+
+  )
+};
+//struggling big time. don't think i have enough experience w/ conditionals to manage this.
+
+//Invocation of function:
+
+//Expected input is DOGS AGE aka our argument
+dogAgeCalculator(insertARGUMENT);
+
+//Expected output of this function is HUMAN YEARS and should look like this:
+// (STRING) "Your dog is {num} years in human"
+
+//option to do either return or console.log, but it has to return expected result.
+
+//How old is my dog in human years
+
+//~~~~~~~~~~~~~~~~~~~~
+const myAge = 25
+//this is my age as a number
+
+var earlyYears = 2
+//used var so that this is able to change, the first two years of a dogs life count as 10.5 years
+
+earlyYears = (2 * 10.5)
+console.log(earlyYears)
+
+let laterYears = myAge - 2;
+//myAge minus 2 due to accounting for the first two years
+
+laterYears = (23 * 4)
+
+//calculating dog years by later years
+//according to online conversion, i should be 113, not 92. did not print early and later
+
+//YAY!! 
+var myAgeInDogYears = (earlyYears + laterYears)
+
+'Kerstin Cynkar'.toLowerCase();
+var myName = 'Kerstin Cynkar'
+//my name stored into variable with all lowercase
+console.log ('My name is ' + myName + '. I am ' + myAge + ' years old in human years which is ' + myAgeInDogYears + ' years old in dog years.')
+//string using the above declared variables

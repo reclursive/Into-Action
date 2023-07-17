eaf494e79cf722215e182bb1c085dd4d328a3058
reclursive <imafraidofferrets@gmail.com>
2023-07-16 19:57:44 -0700

    function assignments

---
+++ Practice-JS-Questions/palindrome.js
@@ -0,0 +1,21 @@
+
+//How old is my dog in human years
+
+const palidromeCheck = (taco) => {
+    //the following will be evaluating whatever STRING is passed as an argument 
+    // for the parameter called taco (you can rename)
+
+    //if statements, you have more than one edge cases for this
+    // you will need built in functions.
+
+}
+
+//Invocation of function:
+
+//Expected input is DOGS AGE aka our argument
+palidromeCheck(insertARGUMENT)
+
+//Expected output of this function is BOOLEAN (either true or false) and will be returned as a string:
+//"This string is NOT a palindrome" or "This string is a palidrome"
+
+//option to do either return or console.log, but it has to return expected result. 
